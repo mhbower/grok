435c83e764bffb9e34705ddbe547556d0c09e59b
Mark Bower <markhbower@gmail.com>
2021-08-25 16:39:46 -0700

    testing

--- chap1.py
+++ chap1.py
@@ -2,9 +2,11 @@
 def binary_search(list, item):
     low = 0
     high = len(list) -1
+    
 
     while low <= high:
         mid = (low + high) // 2
+        print(mid)
         guess = list[mid]
         if guess == item:
             return mid
@@ -14,7 +16,7 @@ def binary_search(list, item):
             low = mid + 1
     return None
 
-my_list = [1,3,5,7,9,11,13,15,17]
+my_list = [2,3,5,7,9,11,13,15,17]
 
 print (binary_search(my_list, 11))
-print (binary_search(my_list, -1))
+## print (binary_search(my_list, -1))
