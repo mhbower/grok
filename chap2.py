7538abe13a817c02f8625c7520e521953ab856e4
Mark Bower <markhbower@gmail.com>
2021-08-25 22:51:14 -0700

    added chap2 exercise

---
+++ chap2.py
@@ -0,0 +1,19 @@
+def findSmallest(arr):
+    #arr will be a list
+    smallest=arr[0]
+    smallest_index=0
+    for i in range(1, len(arr)):
+        if arr[i]<smallest:
+            smallest=arr[i]
+            smallest_index=i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr=[]
+    for i in range(len(arr)):
+        smallest=findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+        print(arr)
+    return newArr
+
+print(selectionSort([5,3,6,12,11,1]))
